25e9c761b476a506133d42d0bda18792680b2ceb
bregydoc <bregy.malpartida@utec.edu.pe>
2017-10-13 21:26:58 -0500

    Fixed a lot of bugs

--- Laboratory_004_uMIPS/micro_MIPS.vhdl
+++ Laboratory_004_uMIPS/micro_MIPS.vhdl
@@ -10,10 +10,10 @@ entity micro_MIPS is
 
 		address: in std_logic_vector(3 downto 0);
 		regDst: in std_logic;
-		wrData: in std_logic;
+		wrData: in std_logic_vector(31 downto 0);
 		regWrite: in std_logic;
 		aluSrc: in std_logic;
-		aluCnt: in std_logic;
+		aluCnt: in std_logic_vector(3 downto 0);
 
 		Z: out std_logic;
 		aluRes: out std_logic_vector(31 downto 0);
@@ -25,8 +25,8 @@ architecture micro_MIPS_arc of micro_MIPS is
 	-----------------------------------------------------------------------------
 	component mux
 		generic (
-			n = 32
-		)
+			n: integer := 32
+		);
 		port (
 			a, b: in std_logic_vector(n-1 downto 0);
 			sel: in std_logic;
@@ -44,22 +44,22 @@ architecture micro_MIPS_arc of micro_MIPS is
 		);
 	end component alu;
 	----------------------------------------------------------------------------
-	component instructions_memory is
+	component instmem is
 		port (
-			address: in std_logic_vector(3 downto 0);
-			data: out std_logic_vector(31 downto 0)
+			inst_address: in std_logic_vector(3 downto 0);
+			instruction: out std_logic_vector(31 downto 0)
 		);
-	end component instructions_memory;
+	end component instmem;
 	----------------------------------------------------------------------------
-	component register_memory is
+	component registers is
 		port (
-	        clock: in std_logic;
+			clock: in std_logic;
 			reg_write: in std_logic;
-			read_reg_1, read_reg_2, write_reg: in std_logic_vector(4 downto 0);
-			write_data: in std_logic_vector(31 downto 0);
-			read_data_1, read_data_2: out std_logic_vector(31 downto 0)
+			R_reg1, R_reg2, W_reg: in std_logic_vector(4 downto 0);
+			W_data: in std_logic_vector(31 downto 0);
+			R_data1, R_data2: out std_logic_vector(31 downto 0)
 		);
-	end component register_memory;
+	end component registers;
 	----------------------------------------------------------------------------
 	component sign_extend is
 		port (
@@ -69,19 +69,19 @@ architecture micro_MIPS_arc of micro_MIPS is
 	end component sign_extend;
 	----------------------------------------------------------------------------
 
-	signal instruction: std_logic_vector(31 downto 0);
-	alias opcode is instruction(31 downto 26);
+	signal instruction_internal: std_logic_vector(31 downto 0);
+	alias opcode is instruction_internal(31 downto 26);
 
 
 	-- For any R type instruction:
-	alias rs is instruction(25 to 21);
-	alias rt is instruction(20 to 16);
-	alias rd is instruction(15 to 11);
-	alias shamt is instruction(10 to 6);
-	alias funct is instruction(5 to 0);
-	-- For load/store and branch instruction:
-	alias address_of_instr is instruction(15:0);
+	alias rs is instruction_internal(25 to 21);
+	alias rt is instruction_internal(20 to 16);
+	alias rd is instruction_internal(15 to 11);
+	alias shamt is instruction_internal(10 to 6);
+	alias funct is instruction_internal(5 to 0);
 	
+	-- For load/store and branch instruction:
+	alias address_of_instr is instruction_internal(15 to 0);
 
 	-- Signals for alu instance:
 	signal alu_fuct: std_logic_vector(3 downto 0) := "0000";
@@ -93,7 +93,6 @@ architecture micro_MIPS_arc of micro_MIPS is
 	-- Signals for register_memory instance:
 	signal reg_read_data_1: std_logic_vector(31 downto 0);
 	signal reg_write_data: std_logic_vector(31 downto 0);
-	signal alu_in2: std_logic_vector(31 downto 0);
 
 	-- Signals for instruction_memory instance:
 	signal inst_address: std_logic_vector(3 downto 0);
@@ -115,24 +114,25 @@ begin
 		alu_result => aluRes
 	);
 
-	reg_memory: register_memory
+	reg_memory: registers
 	port map (
 		clock => clock,
 		reg_write => regWrite,
-		read_reg_1 => rs,
-		read_reg_2 => rt,		
-		write_data => reg_write_data,
-		read_data_1 => alu_in1,
-		read_data_2 => reg_to_alu_and_mux
+		R_reg1 => rs,
+		R_reg2 => rt,		
+		W_reg => reg_write_data,
+		W_data => wrData,
+		R_data1 => alu_in1,
+		R_data2 => reg_to_alu_and_mux
 	);
 
-	instruction_memory: instructions_memory
+	instruction_memory: instmem
 	port map (
-		address => address,
-		data => instruction
+		inst_address => address,
+		instruction => instruction_internal
 	);
 
-	sign_extend: sign_extend
+	sign_extend_new: sign_extend
 	port map (
 		x => address_of_instr,
 		y => sign_y	
@@ -153,9 +153,8 @@ begin
 		sel => aluSrc,
 		c => mux_alu_c
 	);
-
-	reg_read_reg_1 <= rs;
-	reg_read_reg_2 <= rt;
+	-- reg_read_reg_1 <= rs;
+	-- reg_read_reg_2 <= rt;
 	regOut <= reg_to_alu_and_mux;
 
 end micro_MIPS_arc;
